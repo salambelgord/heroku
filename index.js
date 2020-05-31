4e8e72df40d9b20bd52a609f4e9222b57cf8e8da
salambelgorod <boom000@ya.ru>
2020-05-31 21:32:06 +0300

    second commit

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ bot.on(BotEvents.SUBSCRIBED, response => {
     response.send(new TextMessage(`Hi there ${response.userProfile.name}. I am ${bot.name}! Feel free to ask me anything.`));
 });
 bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
-  if (message.text=="Привет"||"привет"){
+  if (message.text==="Привет"||"привет"){
     response.send(new TextMessage("И вам не хворать."));
   }
     
